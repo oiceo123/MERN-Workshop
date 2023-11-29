47992979d95d1f4b16e1cdb19b022415d8abb491
oiceo123 <iceome44@gmail.com>
2023-11-30 00:15:31 +0700

    เพิ่มฟังก์ชั่น getAllBlog, singleBlog

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,14 +1,19 @@
 // import packages
 const slugify = require("slugify");
 const blogs = require("../models/blogModel");
+const { v4: uuidv4 } = require("uuid");
 
 // ErrCode
 const createErr = "blog-001";
 
 // บันทึกข้อมูล
-exports.create = (req, res) => {
+const create = (req, res) => {
   const { title, content, author } = req.body;
-  const slug = slugify(title);
+  let slug = slugify(title);
+
+  if (!slug) {
+    slug = uuidv4();
+  }
 
   // validate
   switch (true) {
@@ -35,3 +40,20 @@ exports.create = (req, res) => {
     res.status(201).json(result);
   });
 };
+
+// ดึงข้อมูลบทความทั้งหมด
+const getAllBlogs = (req, res) => {
+  blogs.find({}).exec((err, blogs) => {
+    res.status(200).json(blogs);
+  });
+};
+
+// ดึงข้อมูลบทความที่สนใจอ้างอิงตาม slug
+const singleBlog = (req, res) => {
+  const { slug } = req.params;
+  blogs.findOne({ slug }).exec((err, blog) => {
+    res.status(200).json(blog);
+  });
+};
+
+module.exports = { create, getAllBlogs, singleBlog };
--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { create } = require("../controllers/blogController");
+const {
+  create,
+  getAllBlogs,
+  singleBlog,
+} = require("../controllers/blogController");
 
+router.get("/blog", getAllBlogs);
+router.get("/blog/:slug", singleBlog);
 router.post("/blog/create", create);
 
 module.exports = router;
