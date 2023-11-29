67af8080a5d24790840af9317617f016bf5b72f6
oiceo123 <iceome44@gmail.com>
2023-11-30 01:52:26 +0700

    ✨ เพิ่มฟังก์ชั่น formatDate แบบ พ.ศ. , แก้ warning

--- client/src/utils/formatDate.js
+++ client/src/utils/formatDate.js
@@ -1,6 +1,11 @@
-import moment from "moment-timezone";
+// ใช้ moment เป็นเวลาในรูปแบบสากล
+/* import moment from "moment-timezone";
 
 const formatDate = (dateTime) =>
-  moment.tz(dateTime, "Asia/Bangkok").format("DD/MM/YYYY HH:mm:ss");
+  moment.tz(dateTime, "Asia/Bangkok").format("DD/MM/YYYY HH:mm:ss"); */
+
+// ไม่ใช้ moment เป็นเวลาในรูปแบบประเทศไทย
+const formatDate = (dateTime) =>
+  new Date(dateTime).toLocaleString("th-TH", { timeZone: "Asia/Bangkok" });
 
 export default formatDate;
