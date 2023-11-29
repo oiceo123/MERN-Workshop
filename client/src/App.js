4e5d6d8343bf3d79d251c776dd58290c138f0e51
oiceo123 <iceome44@gmail.com>
2023-11-30 00:11:13 +0700

    ✨ เพิ่มหน้าแสดง blog ทั้งหมด

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,47 @@
+import NavbarComponent from "./components/NavbarComponent";
+import axios from "./api";
+import { useState, useEffect } from "react";
+import formatDate from "./utils/formatDate";
+import { Link } from "react-router-dom";
+
 function App() {
+  const [blogs, setBlogs] = useState([]);
+
+  const fetchData = () => {
+    axios
+      .get("/blog")
+      .then((res) => {
+        setBlogs(res.data);
+      })
+      .catch((err) => {
+        alert((err) => alert(err));
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="container">
-      <h1>MERN STACK | Workshop</h1>
-      <button className="btn btn-primary">Bootstrap</button>
+    <div className="container p-5">
+      <NavbarComponent />
+      {blogs.map((blog, index) => (
+        <div
+          className="row"
+          key={index}
+          style={{ borderBottom: "1px solid silver" }}
+        >
+          <div className="col pt-3 pb-2">
+            <Link to={`/blog/${blog.slug}`}>
+              <h2>{blog.title}</h2>
+            </Link>
+            <p>{blog.content.substring(0, 250)}</p>
+            <p className="text-muted">
+              ผู้เขียน: {blog.author} เผยแพร่ : {formatDate(blog.createdAt)}
+            </p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
