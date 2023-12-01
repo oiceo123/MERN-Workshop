59aea736a04cbd2a91603fd884efac2ccce38cd0
oiceo123 <iceome44@gmail.com>
2023-12-02 01:34:13 +0700

    🐛 แก้การแสดงผลวัน ให้แสดงเป็นเลข 2 หลัก

--- client/src/utils/formatDate.js
+++ client/src/utils/formatDate.js
@@ -5,7 +5,19 @@ const formatDate = (dateTime) =>
   moment.tz(dateTime, "Asia/Bangkok").format("DD/MM/YYYY HH:mm:ss"); */
 
 // ไม่ใช้ moment เป็นเวลาในรูปแบบประเทศไทย
+/* const formatDate = (dateTime) =>
+  new Date(dateTime).toLocaleString("th-TH", {
+    timeZone: "Asia/Bangkok",
+  }); */
+
 const formatDate = (dateTime) =>
-  new Date(dateTime).toLocaleString("th-TH", { timeZone: "Asia/Bangkok" });
+  new Date(dateTime).toLocaleString("th-TH", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
 
 export default formatDate;
