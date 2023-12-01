96e024f038102f6262e0fce26a13f883e4d5ac34
oiceo123 <iceome44@gmail.com>
2023-12-02 01:30:43 +0700

    ✨ เพิ่ม interceptors เพื่อ set token

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,20 @@
 import axios from "axios";
+import { getToken } from "../services/authenticate";
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API,
 });
+
+instance.interceptors.request.use(
+  (config) => {
+    if (getToken()) {
+      config.headers["Authorization"] = `Bearer ${getToken()}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
